3610d8f85410b14d06b4fd709ae5cfff7c2e6d52
Riya <sharmariya40624@gmail.com>
2025-09-13 13:04:50 +0530

    First Commit

---
+++ choose_your_own_adventure.py
@@ -0,0 +1,38 @@
+name = input("Type your name:")
+print("welcome",name,"to this adventure!")
+
+answer = input("You are on a dirt road, it has come to an end and you can go left or right. Which way would you ike to go? ").lower()
+
+if answer == "left":
+     answer = input("You come to a river, you can walk around it or swim across? Type walk to walk around and swim to swim across:")
+
+     if answer == "swim":
+       print("you swam across and were eaten by an alligator.")
+       
+     elif answer == "walk":
+       print("you walked for many miles, ran out of water and you lost the game.")
+   
+     else:
+       print("Not a valid option. You lose.")
+
+elif answer == "right":
+    answer = input("You come to a bridge, it looks wobbly, do you want to cross it or head back?")
+
+    if answer == "back":
+      print("You go back and lose.")
+
+    elif answer == "cross":
+       answer = input("You cross a bridge and meet a stranger. Do you talk to them (yes/no)? ")
+
+    if answer == "yes":
+       print("You talk to the stranger and they give you gold. You win!")
+       
+    elif answer == "no":
+       print("You ignore to the stranger and they are offended and you lose.")
+       
+    else:
+       print("Not a valid option. You lose.")
+
+
+print("Thank you for trying", name)
+
---
+++ quizgame.py
@@ -0,0 +1,41 @@
+print("welcome to my computer quiz!")
+ 
+playing = input("do you want to play?")
+
+if playing != "yes":
+    quit()
+
+print ("Okay! Let's play")
+score = 0
+
+answer = input("What does CPU stand for?")
+if answer == "central processing unit":
+    print('Correct!')
+    score += 1 
+else: 
+    print("Incorrect!")
+
+answer = input("What does GPU stand for?")
+if answer == "graphics processing unit":
+    print('Correct!')
+    score += 1
+else: 
+    print("Incorrect!")
+
+answer = input("What does RAM stand for?")
+if answer == "random access memory":
+    print('Correct!')
+    score += 1
+else: 
+    print("Incorrect!")
+
+answer = input("What does PSU stand for?")
+if answer == "power supply":
+    print('Correct!')
+    score += 1
+else: 
+    print("Incorrect!")
+
+print ("You got " + str(score) + "questions correct!")
+print ("You got" + str((score/4) * 100)+"%.")
+
